0d67ddc8e27bfa7a49e762f3ea6b1d485886b281
Iwanttomorrowtocome <white54brack@yahoo.co.jp>
2024-12-12 16:15:32 +0900

    Revert "ssue1-3用"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,9 +34,16 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      @calendar = { title: 'Meeting', date: '2023-12-12' }
       @week_days.push(days)
     end
 
+  end
+end
+
+class TechCamp # クラス名を修正
+  def get_week # メソッド名を修正
+    user_name = "P" # 変数名を修正
+    return user_name
   end
 end
